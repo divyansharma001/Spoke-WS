57f3b556cdfbdc673594c727d334611da44a08ac
Divyansh Sharma <divyanshsharma4507@gmail.com>
2025-02-02 18:17:55 +0530

    Added room logic

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,26 @@
-import { WebSocketServer } from "ws";
+import { WebSocketServer, WebSocket } from "ws";
 
 const wss = new WebSocketServer({ port: 8080 });
 
 let userCount = 0;
+let allSockets: WebSocket[] = [];
+
 
 wss.on("connection", (socket) => {
+    allSockets.push(socket);
+
     userCount++;
     console.log("User connected #" + userCount);
     
+    socket.on("message", (message)=>{
+        console.log("Received: " + message.toString());
+        for(let i=0;i<allSockets.length;i++){
+            const s = allSockets[i];
+            s.send("Sent from server: " + message.toString());
+        }
+        // setTimeout(()=>{
+        //     socket.send("Sent from server: " + message.toString());
+        // }, 1000);
+        
+    })
 });
